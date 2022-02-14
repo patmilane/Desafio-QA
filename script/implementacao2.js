1661ec83e7fa4af9a9a56cdb7799a2901e189146
patmilane <patriciahmilane@hotmail.com>
2022-02-14 08:44:24 -0300

    corrigindo requisito da implementação 2

--- script/implementacao2.js
+++ script/implementacao2.js
@@ -9,19 +9,24 @@ function numerosPrimos() {
     var n = Number(document.getElementById("numero").value);
     var result = document.querySelector("#resultado");
 
-    if (n == 1 || n == 2 || n == 3 || n == 5 || n == 7) {
 
-        if (n == 1) {
+    if(n >= 1 && n <= 1000 ){
+        if (n == 1 || n == 2 || n == 3 || n == 5 || n == 7) {
+
+            if (n == 1) {
+                result.innerHTML = `O número não é primo!`;
+            } else {
+                result.innerHTML = `O número é primo!`;
+            }
+
+        } else if (n % 2 == 0 || n % 3 == 0 || n % 5 == 0 || n % 7 == 0) {
             result.innerHTML = `O número não é primo!`;
+
         } else {
             result.innerHTML = `O número é primo!`;
         }
-
-    } else if (n % 2 == 0 || n % 3 == 0 || n % 5 == 0 || n % 7 == 0) {
-        result.innerHTML = `O número não é primo!`;
-
-    } else {
-        result.innerHTML = `O número é primo!`;
-    }
+    }else{
+        result.innerHTML = `<center>Digite um numero entre 1 e 1000<center>`;
+        }
 }
 
