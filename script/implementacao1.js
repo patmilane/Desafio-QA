dcdd782c976ce7fca90a44d90168a5c900670e55
patmilane <patriciahmilane@hotmail.com>
2022-02-14 08:39:18 -0300

    inserindo requisito no exercicio 1

--- script/implementacao1.js
+++ script/implementacao1.js
@@ -9,10 +9,15 @@ function parOuImpar() {
     var numero = Number(document.getElementById("numero").value);
     var result = document.querySelector("#resultado");
 
-    if (numero % 2 == 0) {
-        result.innerHTML = `O número digitado é par!`
+    if(numero >= 1 && numero <= 1000){
+        if (numero % 2 == 0) {
+        result.innerHTML = `O número digitado é par!`;
 
+        } else {
+        result.innerHTML = `O número digitado é impar!`;
+        }
     } else {
-        result.innerHTML = `O número digitado é impar!`
+        result.innerHTML = `<center>Digite um numero entre 1 e 1000<center>`;
     }
 }
+
